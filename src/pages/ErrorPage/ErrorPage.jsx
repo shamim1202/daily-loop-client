ec767063933e1347c4c4745d435f959d2d875fbc
shamim1202 <mehedi.farzu@gmail.com>
2025-11-10 03:01:06 +0600

    modified error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { FaArrowLeft } from "react-icons/fa";
 import { Link } from "react-router";
 import { usePageTitle } from "../../hooks/usePageTitle";
 
@@ -6,8 +7,8 @@ const ErrorPage = () => {
   usePageTitle("Error- Page not found");
 
   return (
-    <main className="min-h-screen flex items-center justify-center bg-linear-to-r from-sky-100 via-pink-100 to-violet-100 p-6">
-      <section className="max-w-6xl w-full bg-linear-to-r from-sky-50 via-pink-50 to-violet-50 rounded-2xl shadow-lg overflow-hidden grid grid-cols-1 md:grid-cols-2">
+    <main className="min-h-screen flex items-center justify-center bg-linear-to-r from-blue-100 via-purple-100 to-green-100 p-6">
+      <section className="max-w-6xl w-full bg-linear-to-r from-blue-50 via-purple-50 to-green-50 rounded-2xl shadow-lg overflow-hidden grid grid-cols-1 md:grid-cols-2">
         {/* Illustration */}
         <div className="flex items-center justify-center p-4 md:p-12">
           <motion.div
@@ -31,7 +32,7 @@ const ErrorPage = () => {
 
         {/* Text & actions */}
         <div className="p-4 md:p-12 flex flex-col justify-center text-center md:text-left">
-          <h1 className="text-xl md:text-4xl font-semibold md:font-extrabold text-gray-900 dark:text-gray-100">
+          <h1 className="text-xl md:text-4xl font-semibold md:font-extrabold text-[e3e3e3] dark:text-gray-100">
             Oops! Page not found
           </h1>
           <p className="mt-3 md:mt-5 text-xs md:text-base text-gray-600 dark:text-gray-300">
@@ -40,9 +41,9 @@ const ErrorPage = () => {
           <div className="mt-3 md:mt-8">
             <Link
               to="/"
-              className="text-xs md:text-base inline-flex items-center justify-center px-3 py-1.5 md:px-6 md:py-3 rounded-full bg-indigo-600 hover:bg-indigo-700 text-white font-medium shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="btn btn-outline btn-primary btn-xs md:btn-md rounded-4xl"
             >
-              Go Home
+             <FaArrowLeft></FaArrowLeft> Go Home
             </Link>
           </div>
         </div>
